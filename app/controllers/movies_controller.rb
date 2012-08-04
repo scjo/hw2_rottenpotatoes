608d933e817903920cd722ae8dec5a154f585061
Ubuntu <ubuntu@ip-10-227-51-116.eu-west-1.compute.internal>
2012-08-04 14:51:46 +0000

    part2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
 
+  def initialize
+    @ratings = []
+    super
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,11 +12,23 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
 
-    if params[:sort_by]
-      @movies.sort! { |a,b| a.title.downcase <=> b.title.downcase }
-      @title_class = "hilite"
+    @all_ratings = Movie.all_ratings
+    @header_class={}
+
+    if params[:ratings]
+      @ratings = params[:ratings]
+
+      if @ratings.is_a?(Hash)
+        @ratings = @ratings.keys
+      end
+    end
+
+    if ["title", "rating", "release_date"].include? params[:sort_by]
+      @movies = Movie.find(:all, :order => params[:sort_by], :conditions => {:rating => @ratings})
+      @header_class[:sort_by] = "hilite"
+    else
+      @movies = Movie.find(:all, :conditions => {:rating => @ratings})
     end
   end
 
